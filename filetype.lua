d853ca6381d60263f0bce6a71baba7800887ae93
ToonBettens <toon.bettens@outlook.com>
2025-10-21 14:42:52 +0200

    skill and spectre syntax

---
+++ filetype.lua
@@ -0,0 +1,8 @@
+vim.filetype.add({
+  extension = {
+    il = "skill",
+    ils = "skill",
+    ocn = "skill",
+    scs = "spectre",
+  }
+})
--- init.lua
+++ init.lua
@@ -1,18 +1,19 @@
 -- Bootstrap 'mini.nvim'
-local path_package = vim.fn.stdpath('data') .. '/site'
-local mini_path = path_package .. '/pack/deps/start/mini.nvim'
+local package_path = vim.fn.stdpath('data') .. '/site'
+local mini_path = package_path .. '/pack/deps/start/mini.nvim'
+
 if not vim.uv.fs_stat(mini_path) then
-  vim.cmd.echo('"Installing `mini.nvim`" | redraw"')
-  local clone_cmd = {
-    'git', 'clone', '--filter=blob:none',
-    -- Uncomment next line to use "stable" branch
-    -- '--branch', 'stable',
-    'https://github.com/nvim-mini/mini.nvim', mini_path
-  }
+  vim.cmd.echo('"Installing mini.nvim"')
+  local clone_cmd = {'git', 'clone', '--filter=blob:none', 'https://github.com/nvim-mini/mini.nvim', mini_path}
   vim.fn.system(clone_cmd)
-  vim.cmd.packadd('"mini.nvim | helptags ALL"')
-  vim.cmd.echo('"Installed `mini.nvim`" | redraw"')
+  vim.cmd.packadd('mini.nvim')
+  vim.cmd.helptags('ALL')
+  vim.cmd.echo('"Installed `mini.nvim`"')
+  vim.cmd.redraw()
 end
+
+-- Ensure mini.nvim is loaded from your packaging directory
+vim.opt.rtp:prepend(mini_path)
 
 -- Set up 'mini.deps' immediately to have its `now()` and `later()` helpers
 require('mini.deps').setup()
